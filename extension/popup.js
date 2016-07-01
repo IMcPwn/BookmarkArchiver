591d79d316b1a4fc73404764cdd0d12996f22ce2
IMcPwn <cs@imcpwn.com>
2016-07-01 18:39:10 -0400

    Add bookmark archive support

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,22 @@
-// Wayback get URL api: https://archive.org/wayback/available?url=imcpwn.com
+/* BookmarkArchiver - Archive your bookmarks with archive.org
+ * For the latest code and contact information visit: http://imcpwn.com
+
+ * Copyright 2016 IMcPwn 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function archive(url) {
-
     var request = new XMLHttpRequest();
     request.open('GET', "https://web.archive.org/save/" + url);
     request.send();
@@ -10,20 +25,58 @@ function archive(url) {
         var xhr = event.target;
 
         if (xhr.readyState === 4 && xhr.status === 200) {
-            renderStatus("Archived: " + url);
-        } else {
-            renderStatus("Error: Unable to archive: " + url);
+            renderStatus("<b>Archived</b>\n" + url);
+        } else if (xhr.readyState === 4) {
+            renderStatus("<b>Error archiving</b>\n" + url);
         }
     };
 }
 
+/*
+function getWayback(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', "https://archive.org/wayback/available?url=" + url);
+    request.send();
+
+    request.onreadystatechange = function(event) {
+        var xhr = event.target;
+
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Parse JSON and validate
+        } else {
+            callback(false);
+        }
+    };
+}
+*/
+
+function clearResults() {
+    document.getElementById('resultsMessage').innerHTML = ""
+    document.getElementById('status').textContent = "";
+}
+
 function renderStatus(statusText) {
-    document.getElementById('status').textContent = statusText;
+    if (document.getElementById('resultsMessage').innerHTML == "") {
+        document.getElementById('resultsMessage').innerHTML = "<h4>Results<br>--------</h4>";
+    }
+    var row = document.getElementById('status').insertRow(-1);
+    row.innerHTML = statusText;
+}
+
+function isChecked() {
+    if (document.getElementById('saveResult').checked) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function archiveCurrentPage() {
-    getCurrentTabUrl(function(callback) {
-        archive(callback);
+    getCurrentTabUrl(function(bookmark) {
+        archive(bookmark);
+        if (isChecked()) {
+            createBookmark(bookmark.title, bookmark.url);
+        }
     });
 }
 
@@ -42,11 +95,49 @@ function getCurrentTabUrl(callback) {
     });
 }
 
+function archiveBookmarks() {
+    getBookmarks(function(bookmarks) {
+        archiveBookmark(bookmarks);
+    });
+}
+
+function archiveBookmark(bookmarks) {
+    bookmarks.forEach(function(bookmark) {
+        if (bookmark.url) {
+            archive(bookmark.url);
+            if (isChecked()) {
+                createBookmark(bookmark.title, bookmark.url);
+            }
+        }
+        if (bookmark.children) {
+            archiveBookmark(bookmark.children); 
+        }
+    });
+}
+
+function getBookmarks(callback) {
+    chrome.bookmarks.getTree(function(bookmarks) {
+        callback(bookmarks);
+    });
+}
+
+/*
+function createBookmark(title, url, callback) {
+    var d = new Date();
+    var date = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
+    chrome.bookmarks.create({'parentId': "2", 'title': 'ArchivedBookmarks-' + date}, callback);
+    chrome.bookmarks.create({'parentId': "2", 'title': title, 'url': url}, callback);
+}
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('bookmarksButton').onclick = function() {
         archiveBookmarks();
     };
     document.getElementById('currentPageButton').onclick = function() {
         archiveCurrentPage();
+    };
+    document.getElementById('clearResults').onclick = function() {
+        clearResults();
     };
 });
